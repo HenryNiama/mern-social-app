3b55684312df9c76307e61c46b79e714840f4294
Henry Niama Rodríguez <48260750+HenryNiama@users.noreply.github.com>
2022-06-08 15:09:00 -0500

    Creacion del routing en el server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://henrydb:1720@cluster0.o2ycpai.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} no se conectó`));
+
+  // We dont get any warnings in the console: 
+//   mongoose.set('useFindAndModify', false);
